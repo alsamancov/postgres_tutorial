803b2e8a76419aa8e178ff085c64883ecc0c0ca0
Alexey <oleksii.samantsov@agrocore.com.ua>
2018-03-01 22:38:32 +0200

    "Postgresql tutorial task" "home commit with inner join lesson"

--- queries_dvdrental.sql
+++ queries_dvdrental.sql
@@ -103,3 +103,59 @@ SELECT first_name || ' ' || last_name FROM customer ORDER BY first_name || ' ' |
 SELECT first_name || ' ' || last_name AS full_name FROM customer ORDER BY full_name;
 
 --Introduction to PostgreSQL INNER JOIN clause
+
+SELECT
+customer.customer_id,
+first_name,
+last_name,
+email,
+amount,
+payment_date
+FROM
+customer
+INNER JOIN
+payment
+ON
+payment.customer_id = customer.customer_id;
+
+SELECT
+customer.customer_id,
+first_name,
+last_name,
+email,
+amount,
+payment_date
+FROM
+customer
+INNER JOIN payment ON payment.customer_id = customer.customer_id
+ORDER BY
+customer.customer_id;
+
+SELECT
+customer.customer_id,
+first_name,
+last_name,
+email,
+amount,
+payment_date
+FROM
+customer
+INNER JOIN payment ON payment.customer_id = customer.customer_id
+WHERE customer.customer_id = 2;
+
+SELECT
+customer.customer_id,
+customer.first_name customer_first_name,
+customer.last_name customer_last_name,
+customer.email,
+staff.first_name staff_first_name,
+staff.last_name staff_last_name,
+amount,
+payment_date
+FROM
+customer
+INNER JOIN payment ON payment.customer_id = customer.customer_id
+INNER JOIN staff ON payment.staff_id = staff.staff_id;
+
+--PostgreSQL FULL OUTER JOIN
+
